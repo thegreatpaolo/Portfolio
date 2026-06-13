8237db1bbb661ec62e67e2a3ddd83b87001dfcf8
Gian Paolo Oga <gpdoga@gmail.com>
2026-06-13 22:33:40 +0800

    Updated the pdf

--- src/assets/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as courtImage } from './wallstreet.png';
-export { default as webImage1 } from './web1.png';
-export { default as webImage2 } from './web2.png';
-export { default as Kaboooom } from './web5.png';
-export { default as webImage3 } from './web3.png';
-export { default as webImage4 } from './web4.png';
---
+++ src/assets/index.tsx
@@ -0,0 +1,16 @@
+import courtImage from './wallstreet.png';
+import webImage1 from './web1.png';
+import webImage2 from './web2.png';
+import Kaboooom from './web3.png';
+import webImage3 from './web4.png';
+import webImage4 from './web5.png';
+
+
+export {
+  courtImage,
+  webImage1,
+  webImage2,
+  Kaboooom,
+  webImage3,
+  webImage4
+};
